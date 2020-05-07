a6cb63a03496f1f6be43c04614d8fb4f1ef7a987
Richard Cho <96richardohc@gmail.com>
2020-05-07 01:39:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,16 @@
 
 def countdown(counter)
   while counter > 0 
-  puts "#{counter} SECOND(S)!"
+    puts "#{counter} SECOND(S)!"
   counter -= 1
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(counter)
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+  end
+  "HAPPY NEW YEAR!"
  
